cbc580723b9294c8624216ec1624944b41c8dd11
Filipe <162770790+filoroch@users.noreply.github.com>
2025-02-11 20:54:05 +0000

    feeat(exercise) aprendendo sobre area
    Signed-off-by: Filipe <162770790+filoroch@users.noreply.github.com>

---
+++ 1012_Area/area.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva um programa que leia 03 numeros de pontos flutuante (floats).
+ * Calcule:
+ * a. a area do retangulo triangulo de base A e peso C
+ * b. a area do raio do circulo. PI 
+ * c. a area do trapezio que tem A e B como base e C como peso;
+ * d. a area do quadrado de lado B;
+ * e. a area do retangulo com lados A e B;
+ */
